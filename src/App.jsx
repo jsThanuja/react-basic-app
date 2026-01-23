dbbe820dd832a33537b4fda661daadb5837e1e89
jsThanuja <jsthanuja23@gmail.com>
2026-01-23 22:14:43 +0530

    Initial React app setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <Header />
+      <h1>My First React App</h1>
+      <p>React is working successfully.</p>
+    </div>
+  );
+}
+
+export default App;
